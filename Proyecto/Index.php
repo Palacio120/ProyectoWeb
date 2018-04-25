4f2b8606fccafa64b1188d5d1dc4fa04bd36ef38
Palacio120 <alejandropalacio119@gmail.com>
2018-04-24 21:11:12 -0500

    Cambios

--- Proyecto/Index.php
+++ Proyecto/Index.php
@@ -9,25 +9,33 @@
         </ol>
         <div class="carousel-inner" role="listbox">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="imagenes/banner1.jpg" alt="First slide">
+            <img class="d-block" height="600px" width="100%" src="imagenes/banner1.jpg" alt="First slide" >
           </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="imagenes/banner2.png" alt="Second slide">
+          <div class="carousel-item">
+            <img class="d-block" height="600px" width="100%" src="imagenes/banner2.png" alt="Second slide">
           </div>
-          <div class="carousel-item ">
-            <img class="d-block w-100" src="imagenes/banner3.jpg" alt="Third slide">
+          <div class="carousel-item">
+            <img class="d-block" height="600px" width="100%" src="imagenes/banner3.jpg" alt="Third slide" >
+            <div class="container">
+                <div class="carousel-caption">
+                  <h1 style="margin-bottom: 100px;">Accesorios Elementales</h1>
+                </div>
+            </div>
           </div>
         </div>
-        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left"></span>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
           <span class="sr-only">Anterior</span>
         </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Siguiente</span>
         </a>
-      </div>
-      <?php w ?>
+      </div class="container">
+      <?php 
+
+
+      ?>
      </div>
 <?php
   include('footer.php')
